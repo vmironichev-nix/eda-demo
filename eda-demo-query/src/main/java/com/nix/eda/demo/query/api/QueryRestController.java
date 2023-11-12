0ec9ce522b088bbb630df34acb76be6dae1d57d3
Valeriy Mironichev <vmironichev@n-ix.com>
2023-11-12 23:23:09 +0200

    update query model api, minor adjustments (#2)

--- eda-demo-query/src/main/java/com/nix/eda/demo/query/api/QueryRestController.java
+++ eda-demo-query/src/main/java/com/nix/eda/demo/query/api/QueryRestController.java
@@ -1,4 +1,32 @@
 package com.nix.eda.demo.query.api;
 
+import com.nix.eda.demo.query.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "orders")
 public class QueryRestController {
+
+    private final OrderService orderService;
+
+    public QueryRestController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity findOrder(@PathVariable("id") UUID id) {
+        return ResponseEntity.of(orderService.findOrder(id));
+    }
+
+    @GetMapping
+    public ResponseEntity getOrders() {
+        return ResponseEntity.ok(orderService.getOrders());
+    }
 }
+
